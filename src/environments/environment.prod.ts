4faa4fb714e5748eb22d662cdae4f072956a2492
Efisio Pitzalis Morillas <git.epitzalis@gmx.com>
2020-05-28 21:12:11 +0200

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_REST_URL: 'http://localhost:3000',
+  API_REST_URL: 'http://balanceadorbookapi-21191277.us-east-1.elb.amazonaws.com:3000',
 };
