e9c1f2f3bfbc723ba319beed636e6f53ead60fe7
epitzalis <epitzalism@gmail.com>
2022-02-23 20:38:47 +0100

    move pipe

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,8 +9,7 @@ import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { NavModule } from '../nav/nav.module';
-import { ReduceTextPipe } from './reduce-text/reduce-text.pipe';
-
+import { ReduceTextPipe } from '../pipes/reduce-text/reduce-text.pipe';
 
 @NgModule({
   imports: [
